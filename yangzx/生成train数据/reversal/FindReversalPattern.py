261a0b08bf2a2f45e0e1b5335f3b7b14e6e92da7
yangwohenmai <zongxianyang@outlook.com>
2022-06-23 17:48:57 +0800

    寻找反转逻辑

--- yangzx/生成train数据/reversal/FindReversalPattern.py
+++ yangzx/生成train数据/reversal/FindReversalPattern.py
@@ -1,6 +1,7 @@
 import tushare as ts
 import datetime as dt
 import time
+import typing
 
 
 # 获取所有股票列表
@@ -25,7 +26,6 @@ def GetStockPool(indexCode='399300.SZ'):
     for col in df.values:
         stockPoolDic[col[1]] = True
     return list(stockPoolDic.keys())
-    
 
 # 寻找反转趋势
 # 1. ma10 连续在 ma20下 n 天后，首次出现交叉，获取这样的数据
@@ -33,9 +33,32 @@ def GetStockPool(indexCode='399300.SZ'):
 # 3. 如果最低点后的 m 个交易日，涨幅达到x or 连续y天 ma10 > ma20，即为所求
 # 4. 记录这样的股票代码和日期区间，输出图像
 
+# 取股票均线数据
+def GetStockMa(stockCode, period=1401, maPara=[10, 20], calDay=100, type="D", benchmark="close"):
+    startdate = (dt.datetime.today() - dt.timedelta(period)).strftime("%Y%m%d")
+    #df = ts.pro_bar(ts_code=stockCode, adj='qfq', freq=type, start_date=startdate, end_date=time.strftime("%Y%m%d"))
+    df = ts.pro_bar(ts_code=stockCode, start_date=startdate, end_date=time.strftime("%Y%m%d"), ma=maPara)
+    
+    # 样本点小于40个不计算
+    if df is None or df.values is None or len(df.values) < 40:
+        print("数据缺失：",stockCode)
+        return 0
+    
+    # 遍历重构正向时序上的数据
+    OrderDic = typing.OrderedDict()
+    for i in range(len(df.values)-1,-1,-1):
+        if i > len(df.values) - maPara[len(maPara)-1]:
+            continue
+        value = df.values[i]
+        OrderDic[value[1]] = {'tdate':value[1], 'open':value[2], 'high':value[3], 'low':value[4], 'close':value[5], \
+            'lclose':value[6], 'change':value[7], 'chg':value[8], 'vol':value[9], 'amount':value[10]*1000, \
+            'ma10':value[11], 'ma_v_10':value[12], 'ma20':value[13], 'ma_v_20':value[14]}
+    print(OrderDic[next(reversed(OrderDic))])
+    return OrderDic
 
 # 主函数
 if __name__ == '__main__':
+    GetStockMa('000001.SZ')
     # 生成策略结果数据
     tagDic = dict()
     # 循环计数器
