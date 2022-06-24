1a95c1eab2743104668cd28349e2a83ca20884b5
yangwohenmai <zongxianyang@outlook.com>
2022-06-24 16:28:25 +0800

    update

--- yangzx/生成train数据/reversal/FindReversalPattern.py
+++ yangzx/生成train数据/reversal/FindReversalPattern.py
@@ -3,6 +3,10 @@ import datetime as dt
 import time
 import typing
 
+import matplotlib.pyplot as plt
+import mpl_finance as mpf
+import numpy as np
+
 
 # 获取所有股票列表
 def GetAllStockList():
@@ -34,7 +38,8 @@ def GetStockPool(indexCode='399300.SZ'):
 # 4. 记录这样的股票代码和日期区间，输出图像
 
 # 取股票均线数据
-def GetStockMa(stockCode, period=1401, maPara=[10, 20], calDay=100, type="D", benchmark="close"):
+# maPara: 想要获取的均线窗口值
+def GetStockMA(stockCode, period=1401, maPara=[10, 20], calDay=100, type="D", benchmark="close"):
     startdate = (dt.datetime.today() - dt.timedelta(period)).strftime("%Y%m%d")
     #df = ts.pro_bar(ts_code=stockCode, adj='qfq', freq=type, start_date=startdate, end_date=time.strftime("%Y%m%d"))
     df = ts.pro_bar(ts_code=stockCode, start_date=startdate, end_date=time.strftime("%Y%m%d"), ma=maPara)
@@ -47,24 +52,73 @@ def GetStockMa(stockCode, period=1401, maPara=[10, 20], calDay=100, type="D", be
     # 遍历重构正向时序上的数据
     OrderDic = typing.OrderedDict()
     for i in range(len(df.values)-1,-1,-1):
+        # 剔除前n天均线为Nan值的数据
         if i > len(df.values) - maPara[len(maPara)-1]:
             continue
         value = df.values[i]
         OrderDic[value[1]] = {'tdate':value[1], 'open':value[2], 'high':value[3], 'low':value[4], 'close':value[5], \
             'lclose':value[6], 'change':value[7], 'chg':value[8], 'vol':value[9], 'amount':value[10]*1000, \
             'ma10':value[11], 'ma_v_10':value[12], 'ma20':value[13], 'ma_v_20':value[14]}
-    print(OrderDic[next(reversed(OrderDic))])
+    #print(OrderDic[next(reversed(OrderDic))])
     return OrderDic
+
+def DrawDataK(dates, prices):
+    #prices = data[['open', 'high', 'low', 'close']]
+    #dates = data['date']
+    candleData = np.column_stack([list(range(len(dates))), prices])
+    fig = plt.figure(figsize=(10, 6))
+    ax = fig.add_axes([0.1, 0.3, 0.8, 0.6])
+    plt.axis('off')
+    mpf.candlestick_ohlc(ax, candleData, width=0.5, colorup='r', colordown='b')
+    plt.savefig('D:\\导出的图片.png')
+    #plt.show()
+
+# 判断反转模式
+def FindReversal(stockMA, reversalDay=8):
+    # 连续趋势计数
+    compareCount = 0
+    # 日期列表
+    seriesTdateList = list()
+    #seriesCloseList = list()
+    # 详情列表
+    seriesList = list()
+    OrderDic = typing.OrderedDict()
+    for stockMAKey, stockMAValue in stockMA.items():
+        # 从下跌趋势开始时，记录本次趋势内数据
+        if float(stockMAValue["ma10"]) <= float(stockMAValue["ma20"]):
+            seriesTdateList.append(stockMAValue["tdate"])
+            #seriesCloseList.append(stockMAValue["close"])
+            seriesList.append([stockMAValue["open"],stockMAValue["high"],stockMAValue["low"],stockMAValue["close"]])
+            compareCount += 1
+        # 遇到死叉，如果本次下跌趋势满足，就进行记录,若记录当天信息，在此处添加即可
+        if float(stockMAValue["ma10"]) > float(stockMAValue["ma20"]):
+            if compareCount > reversalDay:
+                print(stockMAValue["tdate"])
+                print(seriesTdateList)
+                #print(seriesCloseList[-reversalDay])
+                OrderDic[stockMAValue["tdate"]] = {"tdateList":seriesTdateList, "seriesList":seriesList}
+                seriesTdateList = list()
+                #seriesCloseList = list()
+                seriesList = list()
+            compareCount = 0
+    listIndex = list(OrderDic.keys())
+    # 返回用于作图的数据
+    DrawDataK(OrderDic[listIndex[0]]["tdateList"][-reversalDay:], OrderDic[listIndex[0]]["seriesList"][-reversalDay:])
 
 # 主函数
 if __name__ == '__main__':
-    GetStockMa('000001.SZ')
+    
     # 生成策略结果数据
     tagDic = dict()
     # 循环计数器
     dataCount = 0
     # 股票样本数
     sampleCount = 50
+    stockMADic = GetStockMA('000001.SZ')
+    FindReversal(stockMADic, 16)
+
+
+
     # 获取股票池 上证50:000016.SH 沪深300:399300.SZ 上证180：000010.SH
     stockPoolList = GetStockPool('000016.SH')
     # 读取全市场股票列表
---
+++ yangzx/生成train数据/reversal/Helper/DrawHelper.py
@@ -0,0 +1,40 @@
+import tushare as ts
+import matplotlib.pyplot as plt
+import mpl_finance as mpf
+import numpy as np
+
+def DrawDataK(dates, prices):
+    #data = ts.get_k_data('002153', ktype='D', autype='qfq', start='2021-12-17', end='')
+    #prices = data[['open', 'high', 'low', 'close']]
+    #dates = data['date']
+    candleData = np.column_stack([list(range(len(dates))), prices])
+    # 图片的长宽比例
+    fig = plt.figure(figsize=(10, 6))
+    # 图像的左，底，宽，高 位置（left, bottom, width, height）
+    ax = fig.add_axes([0.1, 0.3, 0.8, 0.6])
+    # 是否带边框
+    plt.axis('off')
+    mpf.candlestick_ohlc(ax, candleData, width=0.5, colorup='r', colordown='b')
+    plt.savefig('D:\\导出的图片.png')
+    #plt.show()
+
+
+
+# 主函数
+if __name__ == '__main__':
+    data = ts.get_k_data('002153', ktype='D', autype='qfq', start='2021-12-17', end='')
+    print(data)
+    prices = data[['open', 'high', 'low', 'close']]
+    dates = data['date']
+    candleData = np.column_stack([list(range(len(dates.values.tolist()))), prices.values.tolist()])
+    # 图片的长宽比例
+    fig = plt.figure(figsize=(10, 6))
+    # 图像的左，底，宽，高 位置（left, bottom, width, height）
+    ax = fig.add_axes([0.1, 0.3, 0.8, 0.6])
+    # 是否带边框
+    plt.axis('off')
+    mpf.candlestick_ohlc(ax, candleData, width=0.5, colorup='r', colordown='b')
+    #plt.savefig('D:\\导出的图片.png')
+    plt.show()
+    input()
+    
