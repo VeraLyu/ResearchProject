35955fead7647e83597024e800311c4fc1f3a525
yangwohenmai <zongxianyang@outlook.com>
2022-06-28 10:59:01 +0800

    update

--- yangzx/生成train数据/reversal/3.FindReversalPattern_只生成最高点.py
+++ yangzx/生成train数据/reversal/3.FindReversalPattern_只生成最高点.py
@@ -3,6 +3,7 @@ import datetime as dt
 import time
 import typing
 import sys
+import os
 
 import matplotlib.pyplot as plt
 import mpl_finance as mpf
@@ -14,17 +15,17 @@ from Helper import ZipHelper
 
 """"
 # 寻找反转模式
-# 1. ma10 连续在 ma20下 n 天后，首次出现交叉，获取这样的区间数据（不包含交叉当天）
-# 2. 在交叉日区间中，找到最低点对应的日期
-# 3. 从最低点向前取 m 个交易日
+# 1. ma10 连续在 ma20 上 n 天后，首次出现交叉，获取这样的区间数据（不包含交叉当天）
+# 2. 在交叉日区间中，找到最高点对应的日期
+# 3. 从最高点向前取 m 个交易日
 # 4. 记录股票代码、日期区间、价格区间，输出区间数据的图像
 
 # 后续改进：
-# 1.在交叉区间内，找到最低点对应日期，向后取 m 个交易日
-# 2.若最低点后的 m 个交易日涨幅达到x，即为所求 
+# 1.在交叉区间内，找到最高点对应日期，向后取 m 个交易日
+# 2.若最低点后的 m 个交易日跌幅达到x，即为所求 
 # or
 # 1.在交叉日期后，向后取 m 个交易日
-# 2.若连续 m 天 ma10 > ma20，即为所求
+# 2.若连续 m 天 ma10 < ma20，即为所求
 """
 
 # 获取所有股票列表
@@ -81,28 +82,28 @@ def FindReversal(stockMA, reversalDay=8):
     seriesTdateList = list()
     # 交易数据列表
     seriesList = list()
-    OrderDic = typing.OrderedDict()
+    orderDic = typing.OrderedDict()
     for stockMAKey, stockMAValue in stockMA.items():
-        # 从首次下跌趋势开始时，记录本次趋势内序列
-        if float(stockMAValue["ma10"]) <= float(stockMAValue["ma20"]):
+        # 从首次上涨趋势开始时，记录本次趋势内序列
+        if float(stockMAValue["ma10"]) >= float(stockMAValue["ma20"]):
             seriesTdateList.append(stockMAValue["tdate"])
             seriesList.append([stockMAValue["open"],stockMAValue["high"],stockMAValue["low"],stockMAValue["close"]])
             compareCount += 1
-        # 遇到死叉本轮趋势终止，若本轮下跌趋势满足趋势时长，在OrderDic添加本轮趋势序列（默认不记录死叉当天信息，若记录死叉当天信息，在此处添加即可）
-        if float(stockMAValue["ma10"]) > float(stockMAValue["ma20"]):
+        # 遇到死叉本轮趋势终止，若本轮上涨趋势满足趋势时长，在orderDic添加本轮趋势序列（默认不记录交叉当天信息，若记录交叉当天信息，在此处添加即可）
+        if float(stockMAValue["ma10"]) < float(stockMAValue["ma20"]):
             if compareCount > reversalDay:
-                # 寻找区间最低点的位置
+                # 寻找区间最高点的位置
                 featureList = np.array(seriesList)[:,3].tolist()
                 featureList.reverse()
-                minIndex = featureList.index(min(featureList))
-                theIndex = len(featureList)-minIndex-1
+                maxIndex = featureList.index(max(featureList))
+                theIndex = len(featureList)-maxIndex-1
                 #featureList.reverse()
-                # 只存储最低点之前的序列
-                OrderDic[stockMAValue["tdate"]] = {"tdateList":seriesTdateList[:theIndex+1], "seriesList":seriesList[:theIndex+1]}
+                # 只存储最高点之前的序列
+                orderDic[stockMAValue["tdate"]] = {"tdateList":seriesTdateList[:theIndex+1], "seriesList":seriesList[:theIndex+1]}
             seriesTdateList = list()
             seriesList = list()
             compareCount = 0
-    return OrderDic
+    return orderDic
 
 # 画出每个符合要求的图
 def SavePicture(code, dataDic, reversalDay, reversalDayOffset, dirPath=''):
@@ -120,6 +121,7 @@ def SavePicture(code, dataDic, reversalDay, reversalDayOffset, dirPath=''):
 
 # 主函数
 if __name__ == '__main__':
+    print(os.path.exists("..\\..\\..\\Communal\\ReversalNegative1"))
     # 生成策略结果数据
     tagDic = dict()
     # 循环计数器
@@ -131,9 +133,9 @@ if __name__ == '__main__':
     # 提取趋势区间长度偏移量，剔除最低点右侧数据后，序列提取长度应相对缩短
     reversalDayOffset = 5
     # 压缩文件源路径
-    dirPath = "..\\..\\..\\Communal\\ReversalPositive"
+    dirPath = "..\\..\\..\\Communal\\ReversalNegative"
     # 压缩文件生成路径
-    imgOutFullName = "..\\..\\..\\Communal\\ReversalPositive.zip"
+    imgOutFullName = "..\\..\\..\\Communal\\ReversalNegative.zip"
 
     # 获取股票池 上证50:000016.SH 沪深300:399300.SZ 上证180：000010.SH
     stockPoolList = GetStockPool('000016.SH')
